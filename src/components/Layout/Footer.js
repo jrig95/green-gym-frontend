68bf1667f2dc49a1d8bb65a2fa2b26bda0351347
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-25 21:16:14 +0800

    added icons to footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,13 +1,17 @@
-import classes from './Footer.module.css';
+import { BsFacebook } from "react-icons/bs";
+import { IoLogoWechat } from "react-icons/io5";
+
+import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <div className={classes.container}>
-      <div>
-        <h1>Text</h1>
+      <div className={classes.logoContainer}>
+        <BsFacebook />
+        <IoLogoWechat />
       </div>
     </div>
-  )
+  );
 };
 
 export default Footer;
