c9f00d5540e6edf0d1ba8e07fdbdb668eb49901f
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-25 19:14:59 +0800

    adding spacing to NavBar - created Pages folder

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -2,7 +2,7 @@ import classes from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <div className={classes.container}></div>
+    <div className={classes.container}><h1>Hello</h1></div>
   )
 };
 
