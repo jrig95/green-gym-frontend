268c3febbafaa931d13eb521557ae65b5ee2e7f1
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-25 20:55:36 +0800

    created footer

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,9 +1,18 @@
-import classes from './LandingPage.module.css';
+import classes from "./LandingPage.module.css";
+import Card from "../../components/UI/Card";
+import Footer from "../../components/Layout/Footer";
 
 const LandingPage = () => {
   return (
-    <div className={classes.container}></div>
-  )
+    <div className={classes.background}>
+      <div className={classes.container}>
+        <Card>
+          <h1>Test Text</h1>
+        </Card>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default LandingPage;
---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import classes from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={classes.container}>
+      <div>
+        <h1>Text</h1>
+      </div>
+    </div>
+  )
+};
+
+export default Footer;
