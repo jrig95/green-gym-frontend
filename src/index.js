e09f288325fcc55bc19d45f7078dfa5e3050abbc
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-26 16:46:49 +0800

    added i18next browser langauge detector

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        "welcome_to_react": "Welcome to React!",
+      },
+    },
+    cn: {
+      translation: {
+        "welcome_to_react": "Chinese Translation",
+      },
+    },
+  },
+  lng: "en",
+  fallbackLng: "en"
+});
+
+// Use translation hook call
+const { t } = useTranslation();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
