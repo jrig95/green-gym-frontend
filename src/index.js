4cd7dda7491c5479e08c9985d816cb81b3963d2f
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-26 16:59:25 +0800

    added i18next http backend

--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        "welcome_to_react": "Welcome to React!",
+// the following code is related to language detection and change.
+// do not move for index.js
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          welcome_to_react: "Welcome to React!",
+        },
+      },
+      cn: {
+        translation: {
+          welcome_to_react: "Chinese Translation",
+        },
       },
     },
-    cn: {
-      translation: {
-        "welcome_to_react": "Chinese Translation",
-      },
+    lng: "en",
+    fallbackLng: "en",
+    detection: {
+      order: ["cookie", "localStorage", "htmlTag", "path", "subdomain"],
+      caches: ["cookie"]
     },
-  },
-  lng: "en",
-  fallbackLng: "en"
-});
+  });
 
 // Use translation hook call
 const { t } = useTranslation();
