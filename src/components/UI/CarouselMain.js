b01592a7331aa2602ae9ee0056f871d7dec2f50f
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-29 07:18:09 +0800

    added swiper package to package.json

--- src/components/UI/CarouselMain.js
+++ src/components/UI/CarouselMain.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 import Card from "./Card";
 import CarouselCard from "./CarouselCard";
 
 import classes from "./CarouselMain.module.css";
+
+// uninstall react-multi-careous
 
 const CarouselMain = () => {
   const CustomLeftArrow = ({ onClick }) => {
@@ -37,11 +40,7 @@ const CarouselMain = () => {
     } = rest;
     const carouselItems = [];
 
-    return (
-      <button>
-        {React.Children.toArray(carouselItems)[index]}
-      </button>
-    )
+    return <button>{React.Children.toArray(carouselItems)[index]}</button>;
   };
 
   const responsive = {
@@ -64,43 +63,34 @@ const CarouselMain = () => {
 
   return (
     <Card className={classes.container}>
-      <Carousel
-        customLeftArrow={<CustomLeftArrow />}
-        customRightArrow={<CustomRightArrow />}
-        CustomDot={<CustomDot />}
-        swipeable={true}
-        draggable={true}
-        showDots={true}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        autoPlay={true}
-        autoPlaySpeed={6000}
-        keyBoardControl={true}
-        // customTransition="all .5"
-        transitionDuration={6000}
-        // containerClass="carousel-container"
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        // deviceType={this.props.deviceType}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
       >
-        <CarouselCard
-          image="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-          title="Card One"
-          body="this is a test to see if this card is going to work"
-        />
-        <CarouselCard
-          image="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-          title="Card Two"
-          body="this is another test to see if the chard is changing"
-        />
-        <CarouselCard
-          image="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-          title="Card Three"
-          body="this is this third card I've made and I think that it's pretty cool"
-        />
-      </Carousel>
+        <SwiperSlide>
+          <CarouselCard
+            image="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            title="Card One"
+            body="this is a test to see if this card is going to work"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarouselCard
+            image="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            title="Card Two"
+            body="this is another test to see if the chard is changing"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CarouselCard
+            image="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            title="Card Three"
+            body="this is this third card I've made and I think that it's pretty cool"
+          />
+        </SwiperSlide>
+      </Swiper>
     </Card>
   );
 };
