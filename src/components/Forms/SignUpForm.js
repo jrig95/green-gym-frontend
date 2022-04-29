3c9e7edf8e92974048e5548bd4b4e8f88396ad05
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-29 10:45:56 +0800

    created basic sign up form

--- src/components/Forms/SignUpForm.js
+++ src/components/Forms/SignUpForm.js
@@ -1,13 +1,42 @@
-import classes from "./SignUpForm.module.css";
+import classes from "./Form.module.css";
 import FormCard from "./FormCard";
+import Button from '../UI/Button';
 
 const SignUpForm = () => {
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(event);
+  };
+
   return (
     <FormCard title="Sign Up">
-      <form>
-        <div>
-          <label htmlFor="first-name">First name</label>
-          <input type="text" id="first-name"/>
+      <form onSubmit={formSubmitHandler}>
+        <div className={classes.controlGroup}>
+          <div className={classes.formControl}>
+            <label htmlFor="first-name">First Name</label>
+            <input type="text" id="first-name" />
+          </div>
+          <div className={classes.formControl}>
+            <label htmlFor="second-name">Second Name</label>
+            <input type="text" id="second-name" />
+          </div>
+          <div className={classes.formControl}>
+            <label htmlFor="company">Company</label>
+            <input type="text" id="company" />
+          </div>
+          <div className={classes.formControl}>
+            <label htmlFor="email">E-Mail</label>
+            <input type="email" id="email" />
+          </div>
+          <div className={classes.formControl}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className={classes.formActions}>
+            <Button color="blue" size="small">Cancel</Button>
+            <Button size="small" type="submit">Submit</Button>
+          </div>
         </div>
       </form>
     </FormCard>
