3b2790602af3af0934c9c0fca0008b518698b8f5
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-30 09:44:22 +0800

    created program card - changed syle on profile banner so that it wasn't directly effecting ever card style - learn something new everyday

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import classes from './App.module.css';
-import LoginForm from './components/Forms/LoginForm';
-import Layout from './components/Layout/Layout';
-import LandingPage from './Pages/LandingPage/LandingPage';
-import ForgotPassword from './Pages/UserFormPages/ForgotPassword';
-import Login from './Pages/UserFormPages/Login';
-import ResetPassword from './Pages/UserFormPages/ResetPassword';
-import SignUp from './Pages/UserFormPages/SignUp';
-import UpdateProfile from './Pages/UserFormPages/UpdateProfile';
-import ProfilePage from './Pages/ProfilePages/ProfilePage';
-import UpdateProfilePage from './Pages/ProfilePages/UpdateProfilePage';
-import ProfileResetPasswordPage from './Pages/ProfilePages/ProfileResetPasswordPage';
-import ProgramsPage from './Pages/ProgramPages/ProgramsPage';
+import classes from "./App.module.css";
+import LoginForm from "./components/Forms/LoginForm";
+import Layout from "./components/Layout/Layout";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import ForgotPassword from "./Pages/UserFormPages/ForgotPassword";
+import Login from "./Pages/UserFormPages/Login";
+import ResetPassword from "./Pages/UserFormPages/ResetPassword";
+import SignUp from "./Pages/UserFormPages/SignUp";
+import UpdateProfile from "./Pages/UserFormPages/UpdateProfile";
+import ProfilePage from "./Pages/ProfilePages/ProfilePage";
+import UpdateProfilePage from "./Pages/ProfilePages/UpdateProfilePage";
+import ProfileResetPasswordPage from "./Pages/ProfilePages/ProfileResetPasswordPage";
+import ProgramsPage from "./Pages/ProgramPages/ProgramsPage";
+import CarouselMain from "./components/UI/CarouselMain";
+import ProfileBanner from "./components/Profile/ProfileBanner";
+
+const DUMMY_DATA = {
+  user_one: {
+    name: "Darren Lewis",
+    image:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    calories: "14532",
+  },
+};
 
 function App() {
   return (
     <Layout>
       <ProgramsPage />
+      <CarouselMain />
+      <ProfileBanner
+        title="My Profile"
+        calories={DUMMY_DATA.user_one.calories}
+        name={DUMMY_DATA.user_one.name}
+        image={DUMMY_DATA.user_one.image}
+      />
     </Layout>
   );
 }
--- src/Pages/ProgramPages/ProgramsPage.js
+++ src/Pages/ProgramPages/ProgramsPage.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 
+import ProgramCard from "../../components/Program/ProgramCard";
 import Banner from "../../components/Layout/Banner";
 
 const ProgramsPage = () => {
   return (
     <Fragment>
       <Banner title="Our Programs" />
+      <ProgramCard />
     </Fragment>
   );
 };
