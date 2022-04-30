1fa1b4a5ddebbfe7104e94d567dd43aee7dd1a98
Alex Pritchard <alexbeet1234@gmail.com>
2022-04-30 10:38:56 +0800

    added a container with display grid

--- src/Pages/ProgramPages/ProgramsPage.js
+++ src/Pages/ProgramPages/ProgramsPage.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 
+import classes from "./ProgramsPage.module.css";
 import ProgramCard from "../../components/Program/ProgramCard";
 import Banner from "../../components/Layout/Banner";
 
@@ -7,17 +8,93 @@ const DUMMY_DATA = {
   program_one: {
     id: "p1",
     title: "Program",
-    image: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zml0bmVzc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description: "Description of program asdjewfbwef fbewufew weiufbewiubf webfiu asdasdwef w ewfwef  wefew sda."
-  }
-}
-
+    image:
+      "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zml0bmVzc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:
+      "Description of program asdjewfbwef fbewufew weiufbewiubf webfiu asdasdwef w ewfwef  wefew sda.",
+  },
+};
 
 const ProgramsPage = () => {
   return (
     <Fragment>
       <Banner title="Our Programs" />
-      <ProgramCard id={DUMMY_DATA.program_one.id} title={DUMMY_DATA.program_one.title} image={DUMMY_DATA.program_one.image} description={DUMMY_DATA.program_one.description}/>
+      <div className={classes.gridContainer}>
+        <div className={classes.programCardGrid}>
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+          <ProgramCard
+            id={DUMMY_DATA.program_one.id}
+            title={DUMMY_DATA.program_one.title}
+            image={DUMMY_DATA.program_one.image}
+            description={DUMMY_DATA.program_one.description}
+          />
+        </div>
+      </div>
     </Fragment>
   );
 };
