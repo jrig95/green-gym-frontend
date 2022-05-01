956d9180ca5fa228d9252298ecb2345f60c14f0b
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-01 10:34:57 +0800

    created ActivitiesPage and added a alt to img in ProgramPage"

---
+++ src/Pages/WorkoutPages/ActivitesPage.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Banner from "../../components/Layout/Banner";
+import classes from "./ActivitiesPage.module.css";
+
+const ActivitiesPage = () => {
+  return (
+    <Fragment>
+      <Banner title="My Activites" />
+    </Fragment>
+  );
+};
+
+export default ActivitiesPage;
