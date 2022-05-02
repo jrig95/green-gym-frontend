bd90312254e0bd66a4dd4097b48ae2b709cbd343
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-02 10:43:58 +0800

    added rowActice - if an exercise is being performed, the row will turn green

--- src/components/Exercise/ExerciseTrackerCardRow.js
+++ src/components/Exercise/ExerciseTrackerCardRow.js
@@ -1,14 +1,17 @@
 import classes from "./ExerciseTrackerCard.module.css";
 
 const ExerciseTrackerCardRow = () => {
+
+  const rowClasses = false ? `${classes.row} ${classes.rowActive}` : classes.row;
+
   return (
-    <div className={classes.row}>
+    <div className={rowClasses}>
       <p className={classes.exerciseRow}>Push Up - Set 1</p>
       <p className={classes.workRow}>45 secs</p>
       <p className={classes.restRow}>15secs</p>
       <div className={classes.questionContainer}>
         <form>
-          <input type="number" />
+          <input type="number" min={0}/>
         </form>
       </div>
     </div>
