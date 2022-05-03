abe06720f2a8d39266b789c00d209895d565399e
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-03 17:56:17 +0800

    created sort component

--- src/Pages/MemberPages/MembersPage.js
+++ src/Pages/MemberPages/MembersPage.js
@@ -1,8 +1,15 @@
 import classes from "./MembersPage.module.css";
 import AdminBanner from "../../components/AdminComponents/Layout/AdminBanner";
+import { Fragment } from "react";
+import Sort from "../../components/AdminComponents/Members/Sort";
 
 const MembersPage = () => {
-  return <AdminBanner />
+  return (
+    <Fragment>
+      <AdminBanner />
+      <Sort />
+    </Fragment>
+  );
 };
 
 export default MembersPage;
---
+++ src/components/AdminComponents/Members/Sort.js
@@ -0,0 +1,16 @@
+import classes from "./Sort.module.css";
+
+const Sort = () => {
+  return (
+    <div>
+      <form>
+        <label>Sort By</label>
+        <select>
+          <option>Program</option>
+        </select>
+      </form>
+    </div>
+  );
+};
+
+export default Sort;
