f1f10a463d0faafb83e16c402016e80f2619207c
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-03 08:15:30 +0800

    updated profileBanner to include a rewards model - set rewards={true} to activate

--- src/Pages/RewardPages/RewardsPage.js
+++ src/Pages/RewardPages/RewardsPage.js
@@ -2,10 +2,19 @@ import { Fragment } from "react";
 import ProfileBanner from "../../components/Profile/ProfileBanner";
 import classes from "./RewardsPage.module.css";
 
+const DUMMY_DATA = {
+  user_one: {
+    name: "Darren Lewis",
+    image:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    calories: "14532",
+  },
+};
+
 const RewardsPage = () => {
   return (
     <Fragment>
-      <ProfileBanner />
+      <ProfileBanner title="My Rewards" image={DUMMY_DATA.user_one.image} rewards={true} points={23400}/>
     </Fragment>
   );
 };
