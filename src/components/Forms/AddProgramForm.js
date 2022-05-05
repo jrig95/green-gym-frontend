05793d2c0a8f1aa795fad64ec947e96a60745f3b
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-05 20:46:51 +0800

    wrote the add program form - still need to add user feedback

--- src/components/Forms/AddProgramForm.js
+++ src/components/Forms/AddProgramForm.js
@@ -1,14 +1,39 @@
 import FormCard from "./FormCard";
 import classes from "./Form.module.css";
+import Button from "../UI/Button";
 
 const AddProgramForm = () => {
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <FormCard title="Add Program">
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <div className={classes.controlGroup}>
-          <div classname={classes.formControl}>
-            <lable>Program Name</lable>
-            <input type="text"/>
+          <div className={classes.formControl}>
+            <label htmlFor="program_name">Program Name</label>
+            <input type="text" id="program_name" />
+          </div>
+          <div className={classes.formControl}>
+            <label htmlFor="number_of_days">Number Of Days</label>
+            <input type="number" id="number_of_days" min={0} />
+          </div>
+          <div className={classes.formControl}>
+            <label htmlFor="description">Description</label>
+            <input type="text" id="description" />
+          </div>
+          <div className={classes.formControl}>
+            <label htmlFor="cover_image">Cover Image</label>
+            <input type="text" id="cover_image" />
+          </div>
+          <div className={classes.formActions}>
+            <Button color="blue" size="small">
+              Cancel
+            </Button>
+            <Button size="small" type="submit">
+              Add
+            </Button>
           </div>
         </div>
       </form>
