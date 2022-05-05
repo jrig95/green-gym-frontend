e1815fa33ed2741c9e328ea83029b68748c3d23d
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-05 11:10:10 +0800

    user can select a member in the list and it will highlight that memeber perminantly

--- src/components/AdminComponents/Members/Member.js
+++ src/components/AdminComponents/Members/Member.js
@@ -12,13 +12,25 @@ const Member = () => {
     setMemberIsSelected((prevState) => !prevState);
   };
 
+  const memberClasses = memberIsSelected
+    ? `${classes.card} ${classes.cardIsSelected}`
+    : classes.card;
+
   return (
-    <Card className={classes.card} onClick={selectMemberHandler}>
-      <p><b>First Name:</b> Darren</p>
-      <p><b>Last Name:</b> Lewis</p>
-      <p><b>Company:</b> Green Gym</p>
-      <p><b>E-Mail:</b> darren@lewis.com</p>
-      {memberIsSelected ? <BsCheckSquare /> : <BsSquare />}
+    <Card className={memberClasses}>
+      <p>
+        <b>First Name:</b> Darren
+      </p>
+      <p>
+        <b>Last Name:</b> Lewis
+      </p>
+      <p>
+        <b>Company:</b> Green Gym
+      </p>
+      <p>
+        <b>E-Mail:</b> darren@lewis.com
+      </p>
+      <div onClick={selectMemberHandler}>{memberIsSelected ? <BsCheckSquare /> : <BsSquare />}</div>
     </Card>
   );
 };
