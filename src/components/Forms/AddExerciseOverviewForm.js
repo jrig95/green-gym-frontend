52482baf05428709d2b356164f906f670f29ba81
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-07 17:39:23 +0800

    made it so a title input cannot be updated if it has been submitted

--- src/components/Forms/AddExerciseOverviewForm.js
+++ src/components/Forms/AddExerciseOverviewForm.js
@@ -1,15 +1,23 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import classes from "./AddExerciseOverviewForm.module.css";
 import useInput from "./Hooks/use-input";
 
 const AddExerciseOverviewForm = ({ exerciseNumber, getOverviewData }) => {
   const [overviewIsSubmitted, setOverviewIsSubmitted] = useState(false);
+  const [exerciseTitlePlaceHolder, setExerciseTitlePlaceHolder] = useState("Title")
+  const [numberOfRepsPlaceHolder, setNumberOfRepsPlaceHolder] = useState(0)
 
   const textNotEmpty = (value) => value !== "";
 
-  const isNotANumber = (value) => {
+  const numberIsValid = (value) => {
     const number = parseInt(value);
-    return !isNaN(number);
+    const isNumber = !isNaN(number);
+    let isNotZero = false;
+    if (isNumber) {
+      isNotZero = number > 0;
+    }
+    const isTrue = isNumber && isNotZero;
+    return isTrue;
   };
 
   const onSubmit = () => {
@@ -26,6 +34,7 @@ const AddExerciseOverviewForm = ({ exerciseNumber, getOverviewData }) => {
   const {
     value: exerciseTitleValue,
     isValid: exerciseTitleIsValid,
+    hasError: exerciseTitleHasError,
     valueChangeHandler: exerciseTitleChangeHandler,
     inputBlurHandler: exerciseTitleBlurHandler,
     reset: resetExerciseTitle,
@@ -34,21 +43,41 @@ const AddExerciseOverviewForm = ({ exerciseNumber, getOverviewData }) => {
   const {
     value: numberOfSetsValue,
     isValid: numberOfSetsIsValid,
+    hasError: numberOfSetsHasError,
     valueChangeHandler: numberOfSetsChangeHandler,
     inputBlurHandler: numberOfSetsBlurHandler,
     reset: resetNumberOfSets,
-  } = useInput(isNotANumber);
+  } = useInput(numberIsValid);
+
+  const formIsValid = exerciseTitleIsValid && numberOfSetsIsValid;
+  const buttonDisabled = !formIsValid;
+
+  useEffect(() => {
+    if (overviewIsSubmitted) {
+      setExerciseTitlePlaceHolder(exerciseTitleValue);
+      resetExerciseTitle();
+      setNumberOfRepsPlaceHolder(numberOfSetsValue)
+      resetNumberOfSets();
+    }
+  }, [overviewIsSubmitted]);
+
+  const submittedClasses = overviewIsSubmitted
+    ? `${classes.formGrid} ${classes.titlesGrey}`
+    : classes.formGrid;
+
+  const exerciseTitleValueTernery = overviewIsSubmitted ? "" : exerciseTitleValue;
 
   return (
     <Fragment>
-      <div className={classes.formGrid}>
+      <div className={submittedClasses}>
         <div className={classes.number}>{exerciseNumber}</div>
         <div className={classes.title}>
           <label htmlFor={`exercise_${exerciseNumber}`}>Title</label>
           <input
             type="text"
             id={`exercise_${exerciseNumber}`}
-            value={exerciseTitleValue}
+            value={exerciseTitleValueTernery}
+            placeholder={exerciseTitlePlaceHolder}
             onChange={exerciseTitleChangeHandler}
             onBlur={exerciseTitleBlurHandler}
           />
@@ -62,12 +91,16 @@ const AddExerciseOverviewForm = ({ exerciseNumber, getOverviewData }) => {
             min={0}
             id={`exercise_${exerciseNumber}_sets`}
             value={numberOfSetsValue}
+            placeholder={numberOfRepsPlaceHolder}
             onChange={numberOfSetsChangeHandler}
             onBlur={numberOfSetsBlurHandler}
           />
+          {numberOfSetsHasError && (
+            <p className={classes.invalid}>Must be a number</p>
+          )}
         </div>
         <div className={classes.addButton}>
-          <button onClick={onSubmit} disabled={overviewIsSubmitted}>
+          <button onClick={onSubmit} disabled={buttonDisabled}>
             {overviewIsSubmitted ? "added" : "add"}
           </button>
         </div>
