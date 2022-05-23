eee74dccbbd31f04c17ad7d8d8a52f3ad668d27f
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-23 18:10:44 +0800

    added an admin function to auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,14 +3,23 @@ import React, { useState } from "react";
 const AuthContext = React.createContext({
   token: "",
   isLoggedIn: false,
+  isAdmin: false,
+  setAdmin: () => {},
   login: (token) => {},
   logout: () => {},
 });
 
 export const AuthContextProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const [admin, setAdmin] = useState(false);
 
   const userIsLoggedIn = !!token;
+
+  const adminIsLoggedIn = !!token && admin;
+
+  const adminHandler = () => {
+    setAdmin(true);
+  }
 
   const loginHandler = (token) => {
     setToken(token);
@@ -18,11 +27,14 @@ export const AuthContextProvider = ({ children }) => {
 
   const logoutHandler = () => {
     setToken(null);
+    setAdmin(false);
   };
 
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
+    isAdmin: adminIsLoggedIn,
+    setAdmin: adminHandler,
     login: loginHandler,
     logout: logoutHandler,
   };
