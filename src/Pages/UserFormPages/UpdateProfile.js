7d867690a5048bbe4f1f4a706bddd59ae6201518
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-25 17:39:44 +0800

    added user to upadate form so we can see the previous data

--- src/Pages/UserFormPages/UpdateProfile.js
+++ src/Pages/UserFormPages/UpdateProfile.js
@@ -1,10 +1,25 @@
+import { useContext } from "react";
+
+import { useUser } from "../../components/User/hooks/use-user";
+import AuthContext from "../../context/AuthContext";
 import UpdateProfileForm from "../../components/Forms/UpdateProfileForm";
 import UserFormPageTemplate from "./UserFormPageTemplate";
 
 const UpdateProfile = () => {
+  // get the user's ID
+  const authCtx = useContext(AuthContext);
+
+  console.log(authCtx);
+  console.log('hererewrwe')
+
+  // get the user data.
+  const { data: userData, isLoading: userIsLoading } = useUser(authCtx.userId);
+
+  if (userIsLoading) return <p>Loading...</p>;
+
   return (
     <UserFormPageTemplate>
-      <UpdateProfileForm />
+      <UpdateProfileForm user={userData} />
     </UserFormPageTemplate>
   );
 };
--- src/components/Forms/UpdateProfileForm.js
+++ src/components/Forms/UpdateProfileForm.js
@@ -1,6 +1,9 @@
 import { useTranslation } from "react-i18next";
-import { useEffect, useState } from "react";
 
+import {
+  textNotEmpty,
+  selectIsValid,
+} from "../../utils/input-from-validations";
 import Button from "../UI/Button";
 import useInput from "./Hooks/use-input";
 import FormCard from "./FormCard";
@@ -16,22 +19,15 @@ const DUMMY_DATA = {
     phone_number: "",
     email: "darren@lewis.com",
     fitness_level: "",
-    passions: "" 
+    passions: "",
   },
 };
 
-const UpdateProfileForm = () => {
-  // const [updatedFirstName, setUpdatedFirstName] = useState(
-  //   DUMMY_DATA.user_one.first_name
-  // );
-
-  // const updatedFirstNameChangeHandler = (event) => {
-  //   setUpdatedFirstName(event.target.value);
-  // };
-
+const UpdateProfileForm = ({ user }) => {
+  // Get Translation hook
   const { t } = useTranslation();
 
-  const textNotEmpty = (value) => value.trim() !== "";
+  console.log(user);
 
   const {
     value: firstNameValue,
@@ -40,7 +36,7 @@ const UpdateProfileForm = () => {
     valueChangeHandler: firstNameChangeHandler,
     inputBlurHandler: firstNameBlurHandler,
     reset: resetFirstName,
-  } = useInput(textNotEmpty);
+  } = useInput(textNotEmpty, user.first_name);
 
   const {
     value: lastNameValue,
@@ -49,7 +45,7 @@ const UpdateProfileForm = () => {
     valueChangeHandler: lastNameChangeHandler,
     inputBlurHandler: lastNameBlurHandler,
     reset: resetLastName,
-  } = useInput(textNotEmpty);
+  } = useInput(textNotEmpty, user.last_name);
 
   const {
     value: companyValue,
@@ -58,28 +54,39 @@ const UpdateProfileForm = () => {
     valueChangeHandler: companyChangeHandler,
     inputBlurHandler: companyBlurHandler,
     reset: resetCompany,
-  } = useInput(textNotEmpty);
+  } = useInput(textNotEmpty, user.user_company);
 
   const {
     value: emailValue,
-    setEnteredValue: setEmail,
     isValid: emailIsValid,
     hasError: emailHasError,
     valueChangeHandler: emailChangeHandler,
     inputBlurHandler: emailBlurHandler,
     reset: resetEmail,
-  } = useInput(textNotEmpty);
+  } = useInput(textNotEmpty, user.email);
 
-  // useEffect(() => {
-  //   firstNameChangeHandler(DUMMY_DATA.user_one.first_name);
-  // }, [])
+  const { value: genderValue, valueChangeHandler: genderChangeHandler } =
+    useInput(selectIsValid);
+
+  const {
+    value: fitnessLevelValue,
+    valueChangeHandler: fitnessLevelChangeHandler,
+  } = useInput(selectIsValid, user.user_fitness_level);
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
-    resetFirstName();
-    resetLastName();
-    resetCompany();
+    const user = {
+      email: emailValue,
+      first_name: firstNameValue,
+      last_name: lastNameValue,
+      user_company: companyValue,
+      user_fitness_level: fitnessLevelValue,
+      user_gender: genderValue
+    }
+
+    // Write hook to create user update
+    
   };
 
   const firstNameClasses = firstNameHasError
@@ -127,17 +134,24 @@ const UpdateProfileForm = () => {
               onBlur={lastNameBlurHandler}
             />
             {lastNameHasError && (
-              <p className={classes.errorText}>please enter your second name</p>
+              <p className={classes.errorText}>please enter your last name</p>
             )}
           </div>
 
           <div className={classes.formControl}>
             <label>Age</label>
-            <input type="number" min={18} max={100} placeholder="18" />
+            <input type="number" min={18} max={100} />
           </div>
           <div className={`${classes.formControl} ${classes.customSelect}`}>
-            <label htmlFor="gender">Gender</label>
+            <label
+              value={genderValue}
+              onChange={genderChangeHandler}
+              htmlFor="gender"
+            >
+              Gender
+            </label>
             <select name="gender" id="gender">
+              <option>Selecet...</option>
               <option>Male</option>
               <option>Female</option>
             </select>
@@ -147,7 +161,7 @@ const UpdateProfileForm = () => {
             <input
               type="text"
               id="company"
-              value={DUMMY_DATA.user_one.company || companyValue}
+              value={companyValue}
               onChange={companyChangeHandler}
               onBlur={companyBlurHandler}
             />
@@ -178,7 +192,13 @@ const UpdateProfileForm = () => {
           </div>
           <div className={classes.formControl}>
             <label htmlFor="fitness_level">Fitness Level</label>
-            <select name="fitness_level" id="fitness_level">
+            <select
+              value={fitnessLevelValue}
+              onChange={fitnessLevelChangeHandler}
+              name="fitness_level"
+              id="fitness_level"
+            >
+              <option>Select...</option>
               <option>Beginner</option>
               <option>Intermediate</option>
               <option>Advanced</option>
