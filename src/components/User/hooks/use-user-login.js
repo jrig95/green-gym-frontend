1ce872754771f4cf35001b97694cc8e34491c9ae
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-25 08:26:23 +0800

    user log in updated

--- src/components/User/hooks/use-user-login.js
+++ src/components/User/hooks/use-user-login.js
@@ -1,26 +1,34 @@
 import axios from "axios";
+import { useContext } from "react";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { useMutation } from "react-query";
 import { userBaseUrl } from "../../../axiosInstance/user-constants";
 import { queryKeys } from "../../../react-query/constants";
 
 const createUserLogin = async (user) => {
-  const { data, headers } = await axios.post(`${userBaseUrl}/login`, { user: user });
+  const { data: response , headers } = await axios.post(`${userBaseUrl}/login`, { user: user });
 
-  return { data, headers };
+  return { response, headers };
 };
 
 export const useUserLogin = () => {
+  const authCtx = useContext(AuthContext);
+
   const { addError } = useAPIError();
   const { mutate } = useMutation(
     queryKeys.user,
     (user) => createUserLogin(user),
     {
       onSuccess: (data) => {
-        console.log(data);
-        // set logged in.
-        // check if admin.
+        const userData = {
+          token: data.headers.authorization,
+          userId: data.response.data.id,
+          admin: data.response.data.admin
+        }
+
+        authCtx.login(userData);
       },
       onError: (error) => {
         console.log(error);
