eb0fca9117cde0a89972ae17a9566f681f91adc1
Alex Pritchard <alexbeet1234@gmail.com>
2022-05-27 13:24:40 +0800

    visiting the activites page when a user doesn't have a program will render a message

--- src/Pages/WorkoutPages/ActivitesPage.js
+++ src/Pages/WorkoutPages/ActivitesPage.js
@@ -1,5 +1,6 @@
 import { Fragment, useEffect, useState, useContext } from "react";
 
+import NoProgram from "../../components/Program/NoProgram";
 import Workout from "../../components/Workout/Workout";
 import AuthContext from "../../context/AuthContext";
 import { useUser } from "../../components/User/hooks/use-user";
@@ -10,12 +11,18 @@ const ActivitiesPage = () => {
   // get the user
   const { data: userData, isLoading: userIsLoading } = useUser(authCtx.userId);
 
+  
+  
+
   if (userIsLoading) return <p>Loading...</p>
+
+  const noProgram = userData.programs.length === 0;
 
   return (
     <Fragment>
       <Banner title="My Activites" />
-      <Workout userData={userData}/>
+      {noProgram && <NoProgram />}
+      {!noProgram && <Workout userData={userData}/>}
     </Fragment>
   );
 };
