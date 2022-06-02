62f3b0df078f2fba7dc7cfb979683b6555f4f13d
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-02 15:07:45 +0800

    exercises that are not complete show as grey

--- src/components/AdminComponents/Members/MemberTrackerCardRow.js
+++ src/components/AdminComponents/Members/MemberTrackerCardRow.js
@@ -2,9 +2,11 @@ import classes from "./MemberTrackerCardRow.module.css";
 
 const MemberTrackerCardRow = ({name, work, rest, reps}) => {
   const exerciseIsCompleted = reps > 0;
+
+  const rowClasses = exerciseIsCompleted ? classes.row : `${classes.row} ${classes.exerciseNotComplete}`
   
   return (
-    <div className={classes.row}>
+    <div className={rowClasses}>
       <p className={classes.exerciseRow}>{name}</p>
       <p className={classes.workRow}>{work}s</p>
       <p className={classes.restRow}>{rest}s</p>
