aa793a215c546bafd4cbe342a30e6d2ad0900c71
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-02 14:06:06 +0800

    moved admin and program check to RewardPages

--- src/Pages/RewardPages/RewardsPage.js
+++ src/Pages/RewardPages/RewardsPage.js
@@ -1,187 +1,20 @@
-import { Fragment, useState, useContext } from "react";
+import { useContext } from "react";
 
+import Rewards from "../../components/Reward/Rewards";
 import AuthContext from "../../context/AuthContext";
-import { useDeleteReward } from "../../components/Reward/hooks/use-delete-reward";
-import { useRewards } from "../../components/Reward/hooks/use-rewards";
-import RewardClaimedMessage from "../../components/Reward/RewardClaimedMessage";
-import ProfileBanner from "../../components/Profile/ProfileBanner";
-import RewardCard from "../../components/Reward/RewardCard";
-import classes from "./RewardsPage.module.css";
-import ClaimReward from "../../components/Reward/ClaimReward";
-import AdminBanner from "../../components/AdminComponents/Layout/AdminBanner";
-import DeleteReward from "../../components/Reward/DeleteReward";
 import { useUser } from "../../components/User/hooks/use-user";
-import Banner from "../../components/Layout/Banner";
 
 const RewardsPage = () => {
   const authCtx = useContext(AuthContext);
-  const deleteReward = useDeleteReward();
   const { data: userData, isLoading: userIsLoading } = useUser(authCtx.userId);
-
-  const noProgram = userData.programs.length === 0;
-
-  const [claimedRewardMessageIsShown, setClaimedRewardMessageIsShown] =
-    useState(false);
-  const [claimRewardIsShown, setClaimRewardIsShown] = useState(false);
-  const [claimedRewardTitle, setClaimedRewardTitle] = useState("");
-  const [claimedRewardPoints, setClaimedRewardPoints] = useState("");
-  const [reward, setReward] = useState({ id: 0, reward_name: "" });
-
-  const [deleteRewardIsShown, setDeleteRewardIsShown] = useState(false);
 
   const admin = authCtx.isAdmin;
 
-  const { data: rewardData } = useRewards();
+  if (userIsLoading) return <p>Loading</p>;
 
-  let programTitle;
-  let programId;
+  const noProgram = userData.programs.length === 0;
 
-  if (!admin && !noProgram) {
-    programTitle = userData.programs[0].program_title;
-    programId = userData.programs[0].id;
-  }
-
-  const programRewardsArray = rewardData.filter(
-    (reward) => parseInt(reward.program_id) === programId
-  );
-  // Create an array based on rewards that do not have a program_id
-  const rewardsArray = rewardData.filter(
-    (reward) => reward.program_id === null
-  );
-
-  const showClaimRewardHandler = (rewardTitle, rewardPoints) => {
-    setClaimedRewardTitle(rewardTitle);
-    setClaimedRewardPoints(rewardPoints);
-    setClaimRewardIsShown(true);
-  };
-
-  const hideClaimRewardHandler = () => {
-    setClaimRewardIsShown(false);
-  };
-
-  const claimRewardHandler = () => {
-    setClaimRewardIsShown(false);
-    setClaimedRewardMessageIsShown(true);
-
-    // Here we need to send an email to the Admin to notify them that a reward has been claimed.
-    console.log(
-      `The user has claimed ${claimedRewardTitle} for ${claimedRewardPoints}`
-    );
-  };
-
-  const hideClaimedRewardMessageHandler = () => {
-    setClaimedRewardMessageIsShown(false);
-  };
-
-  const showDeleteRewardHandler = (reward) => {
-    setReward(reward);
-    setDeleteRewardIsShown(true);
-  };
-
-  const hideDeleteRewardHandler = () => {
-    setDeleteRewardIsShown(false);
-  };
-
-  const deleteRewardHandler = (id) => {
-    deleteReward(id);
-    hideDeleteRewardHandler();
-  };
-
-  // return no program page here
-
-  const programRewards = programRewardsArray.map((reward) => {
-    return (
-      <RewardCard
-        key={reward.id}
-        id={reward.id}
-        admin={admin}
-        title={reward.reward_name}
-        points={reward.reward_points}
-        image={reward.photo_url}
-        onClaimReward={() =>
-          showClaimRewardHandler(reward.reward_name, reward.reward_points)
-        }
-        onDelete={() =>
-          showDeleteRewardHandler({
-            id: reward.id,
-            reward_name: reward.reward_name,
-          })
-        }
-      />
-    );
-  });
-
-  const rewards = rewardsArray.map((reward) => {
-    return (
-      <RewardCard
-        key={reward.id}
-        id={reward.id}
-        admin={admin}
-        title={reward.reward_name}
-        points={reward.reward_points}
-        image={reward.photo_url}
-        onClaimReward={() =>
-          showClaimRewardHandler(reward.reward_name, reward.reward_points)
-        }
-        onDelete={() =>
-          showDeleteRewardHandler({
-            id: reward.id,
-            reward_name: reward.reward_name,
-          })
-        }
-        onUpdate={() => console.log("edit")}
-      />
-    );
-  });
-
-  return (
-    <Fragment>
-      {claimedRewardMessageIsShown && (
-        <RewardClaimedMessage onClose={hideClaimedRewardMessageHandler} />
-      )}
-      {claimRewardIsShown && (
-        <ClaimReward
-          rewardTitle={claimedRewardTitle}
-          rewardPoints={claimedRewardPoints}
-          userPoints={23400}
-          onClose={hideClaimRewardHandler}
-          onClaim={claimRewardHandler}
-        />
-      )}
-      {!admin && !noProgram && (
-        <ProfileBanner
-          title="My Rewards"
-          image={userData.photo_url}
-          rewards={true}
-          points={userData.user_points}
-          calories={userData.user_total_calories}
-        />
-      )}
-      {noProgram && <Banner title="Rewards" />}
-      {admin && <AdminBanner rewards />}
-      {programRewards.length > 0 && (
-        <Fragment>
-          <div className={classes.programRewardsContainer}>
-            <h1 className={classes.programRewardsTitle}>
-              Rewards for {programTitle}
-            </h1>
-            <div className={classes.programRewardsGrid}>{programRewards}</div>
-            <h1 className={classes.rewardsTitle}>General Rewards</h1>
-          </div>
-        </Fragment>
-      )}
-      {deleteRewardIsShown && (
-        <DeleteReward
-          onClose={hideDeleteRewardHandler}
-          onDelete={() => deleteRewardHandler(reward.id)}
-          reward={reward.reward_name}
-        />
-      )}
-      <div className={classes.container}>
-        <div className={classes.rewardsGrid}>{rewards}</div>
-      </div>
-    </Fragment>
-  );
+  return <Rewards userData={userData} admin={admin} noProgram={noProgram} />;
 };
 
 export default RewardsPage;
--- src/components/Reward/Rewards.js
+++ src/components/Reward/Rewards.js
@@ -1,5 +1,187 @@
-const Rewards = () => {
+import { Fragment, useState, useContext } from "react";
 
+import AuthContext from "../../context/AuthContext";
+import { useDeleteReward } from "./hooks/use-delete-reward";
+import { useRewards } from "./hooks/use-rewards";
+import RewardClaimedMessage from "./RewardClaimedMessage";
+import ProfileBanner from "../Profile/ProfileBanner";
+import RewardCard from "./RewardCard";
+import classes from "./Rewards.module.css";
+import ClaimReward from "./ClaimReward";
+import AdminBanner from "../AdminComponents/Layout/AdminBanner";
+import DeleteReward from "./DeleteReward";
+import Banner from "../Layout/Banner";
+
+
+const Rewards = ({ userData, admin, noProgram }) => {
+  const authCtx = useContext(AuthContext);
+  const deleteReward = useDeleteReward();
+
+
+  console.log(noProgram);
+  console.log(!admin);
+  // const noProgram = userData.programs.length === 0;
+
+  const [claimedRewardMessageIsShown, setClaimedRewardMessageIsShown] =
+    useState(false);
+  const [claimRewardIsShown, setClaimRewardIsShown] = useState(false);
+  const [claimedRewardTitle, setClaimedRewardTitle] = useState("");
+  const [claimedRewardPoints, setClaimedRewardPoints] = useState("");
+  const [reward, setReward] = useState({ id: 0, reward_name: "" });
+
+  const [deleteRewardIsShown, setDeleteRewardIsShown] = useState(false);
+
+  const { data: rewardData } = useRewards();
+
+  let programTitle;
+  let programId;
+
+  if (!admin && !noProgram) {
+    programTitle = userData.programs[0].program_title;
+    programId = userData.programs[0].id;
+  }
+
+  const programRewardsArray = rewardData.filter(
+    (reward) => parseInt(reward.program_id) === programId
+  );
+  // Create an array based on rewards that do not have a program_id
+  const rewardsArray = rewardData.filter(
+    (reward) => reward.program_id === null
+  );
+
+  const showClaimRewardHandler = (rewardTitle, rewardPoints) => {
+    setClaimedRewardTitle(rewardTitle);
+    setClaimedRewardPoints(rewardPoints);
+    setClaimRewardIsShown(true);
+  };
+
+  const hideClaimRewardHandler = () => {
+    setClaimRewardIsShown(false);
+  };
+
+  const claimRewardHandler = () => {
+    setClaimRewardIsShown(false);
+    setClaimedRewardMessageIsShown(true);
+
+    // Here we need to send an email to the Admin to notify them that a reward has been claimed.
+    console.log(
+      `The user has claimed ${claimedRewardTitle} for ${claimedRewardPoints}`
+    );
+  };
+
+  const hideClaimedRewardMessageHandler = () => {
+    setClaimedRewardMessageIsShown(false);
+  };
+
+  const showDeleteRewardHandler = (reward) => {
+    setReward(reward);
+    setDeleteRewardIsShown(true);
+  };
+
+  const hideDeleteRewardHandler = () => {
+    setDeleteRewardIsShown(false);
+  };
+
+  const deleteRewardHandler = (id) => {
+    deleteReward(id);
+    hideDeleteRewardHandler();
+  };
+
+  // return no program page here
+
+  const programRewards = programRewardsArray.map((reward) => {
+    return (
+      <RewardCard
+        key={reward.id}
+        id={reward.id}
+        admin={admin}
+        title={reward.reward_name}
+        points={reward.reward_points}
+        image={reward.photo_url}
+        onClaimReward={() =>
+          showClaimRewardHandler(reward.reward_name, reward.reward_points)
+        }
+        onDelete={() =>
+          showDeleteRewardHandler({
+            id: reward.id,
+            reward_name: reward.reward_name,
+          })
+        }
+      />
+    );
+  });
+
+  const rewards = rewardsArray.map((reward) => {
+    return (
+      <RewardCard
+        key={reward.id}
+        id={reward.id}
+        admin={admin}
+        title={reward.reward_name}
+        points={reward.reward_points}
+        image={reward.photo_url}
+        onClaimReward={() =>
+          showClaimRewardHandler(reward.reward_name, reward.reward_points)
+        }
+        onDelete={() =>
+          showDeleteRewardHandler({
+            id: reward.id,
+            reward_name: reward.reward_name,
+          })
+        }
+        onUpdate={() => console.log("edit")}
+      />
+    );
+  });
+
+  return (
+    <Fragment>
+      {claimedRewardMessageIsShown && (
+        <RewardClaimedMessage onClose={hideClaimedRewardMessageHandler} />
+      )}
+      {claimRewardIsShown && (
+        <ClaimReward
+          rewardTitle={claimedRewardTitle}
+          rewardPoints={claimedRewardPoints}
+          userPoints={23400}
+          onClose={hideClaimRewardHandler}
+          onClaim={claimRewardHandler}
+        />
+      )}
+      {!admin && !noProgram && (
+        <ProfileBanner
+          title="My Rewards"
+          image={userData.photo_url}
+          rewards={true}
+          points={userData.user_points}
+          calories={userData.user_total_calories}
+        />
+      )}
+      {noProgram && !admin && <Banner title="Rewards" />}
+      {admin && <AdminBanner rewards />}
+      {programRewards.length > 0 && (
+        <Fragment>
+          <div className={classes.programRewardsContainer}>
+            <h1 className={classes.programRewardsTitle}>
+              Rewards for {programTitle}
+            </h1>
+            <div className={classes.programRewardsGrid}>{programRewards}</div>
+            <h1 className={classes.rewardsTitle}>General Rewards</h1>
+          </div>
+        </Fragment>
+      )}
+      {deleteRewardIsShown && (
+        <DeleteReward
+          onClose={hideDeleteRewardHandler}
+          onDelete={() => deleteRewardHandler(reward.id)}
+          reward={reward.reward_name}
+        />
+      )}
+      <div className={classes.container}>
+        <div className={classes.rewardsGrid}>{rewards}</div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Rewards;
