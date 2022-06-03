c077747cb186aadcb8974d2b4cb4c2f00108d289
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-03 11:31:29 +0800

    added memebers to new addmembers to program message

--- src/Pages/MemberPages/MembersPage.js
+++ src/Pages/MemberPages/MembersPage.js
@@ -11,6 +11,10 @@ import AdminBanner from "../../components/AdminComponents/Layout/AdminBanner";
 import Sort from "../../components/AdminComponents/Members/Sort";
 
 const MembersPage = () => {
+  const [
+    addMemebersToProgramMessageIsShown,
+    setAddMemberToProgramMessageIsShown,
+  ] = useState(false);
   const [noMembersIsShown, setNoMembersIsShown] = useState(false);
   const [searchParam, setSearchParam] = useState("");
   const [programId, setProgramId] = useState(null);
@@ -32,25 +36,26 @@ const MembersPage = () => {
     setNoMembersIsShown(false);
   };
 
+  const hideAddMembersToProgramMessageHandler = () => {
+    setAddMemberToProgramMessageIsShown(false);
+  };
+
   // Write a warning modal that fires if admin has not added users.
   // write a confrimation modal that shows list of users that have been added to the program.
   // Get a list of user names and program name.
 
   useEffect(() => {
-    console.log(addMemebersList);
-
     if (programId != null) {
-      console.log(programId);
       if (addMemebersList.length != 0) {
-        console.log("Add Users to Program")
+        console.log("Add Users to Program");
+        setAddMemberToProgramMessageIsShown(true);
         setProgramId(null);
       } else {
         setNoMembersIsShown(true);
         setProgramId(null);
       }
     }
-  }, [addMemebersList, programId])
-
+  }, [addMemebersList, programId]);
 
   // Create another modal that shows on success. Show have a list of users and Program name.
   // pass the programId via props and call backend for program details
@@ -60,11 +65,15 @@ const MembersPage = () => {
   // On click of add button retreive the members array from the MembersList component
 
   // create hook to get all members
-  const { data: membersData, isLoading, refetch: refetchMembers } = useGetMembers(searchParam);
+  const {
+    data: membersData,
+    isLoading,
+    refetch: refetchMembers,
+  } = useGetMembers(searchParam);
 
   useEffect(() => {
     refetchMembers();
-  }, [searchParam]) 
+  }, [searchParam]);
 
   // render loading
   if (isLoading) return <LoadingSpinnerLarge />;
@@ -78,10 +87,22 @@ const MembersPage = () => {
         addUserProgramId={getAddUserProgramId}
       />
       <div className={classes.container}>
-        {noMembersIsShown && <NoMembersWarning onClose={hideNoMembersWarningHandler}/>}
-        {/* <AddedMembersToProgramMessage /> */}
+        {noMembersIsShown && (
+          <NoMembersWarning onClose={hideNoMembersWarningHandler} />
+        )}
+        {addMemebersToProgramMessageIsShown && (
+          <AddedMembersToProgramMessage
+            onClose={hideAddMembersToProgramMessageHandler}
+            memebers={addMemebersList}
+            programId={programId}
+          />
+        )}
         <Sort />
-        <MembersList members={membersData} programId={programId} fetchAddMembersList={fetchAddMembersListHandler}/>
+        <MembersList
+          members={membersData}
+          programId={programId}
+          fetchAddMembersList={fetchAddMembersListHandler}
+        />
       </div>
     </Fragment>
   );
--- src/components/AdminComponents/Members/AddedMembersToProgramMessage.js
+++ src/components/AdminComponents/Members/AddedMembersToProgramMessage.js
@@ -3,19 +3,25 @@ import Button from "../../UI/Button";
 import Modal from "../../UI/Modal";
 import classes from "./AddedMembersToProgramMessage.module.css";
 
-const AddedMembersToProgramMessage = ({ memebers, programId }) => {
+const AddedMembersToProgramMessage = ({ onClose, memebers, programId }) => {
   // const { data: programData, isLoading: programIsLoading } =
   //   useProgram(programId);
 
+  console.log(memebers);
+  // console.log(programId);
+
+  const memebersList = memebers.map((member) => {
+    return <h3 key={member.id}>{member.name}</h3>;
+  });
+
   return (
-    <Modal>
+    <Modal onClose={onClose}>
       <div className={classes.messageContainer}>
-        <h2>You Have added the following to: </h2>
-        <h1>Message</h1>
-        <p>List of Names</p>
-      </div>
-      <div className={classes.buttonContainer}>
-        <Button>OK</Button>
+        <h2>You Have added the following users to a new Program. </h2>
+        <div>{memebersList}</div>
+        <div className={classes.buttonContainer}>
+          <Button onClick={onClose}>OK</Button>
+        </div>
       </div>
     </Modal>
   );
