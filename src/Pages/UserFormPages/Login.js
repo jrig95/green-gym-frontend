8e8f9411eada355fb84e8d5b1a9f7b40603adb0d
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-07 20:12:02 +0800

    log in and sign up have links to the respctive pages

--- src/Pages/UserFormPages/Login.js
+++ src/Pages/UserFormPages/Login.js
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
+import classes from "./Login.module.css";
 import LoginForm from "../../components/Forms/LoginForm";
 import UserFormPageTemplate from "./UserFormPageTemplate";
 
 const Login = () => {
   return (
     <UserFormPageTemplate>
-      <LoginForm />
+      <div className={classes.loginContainer}>
+        <LoginForm />
+        <Link to="/signup" style={{ textDecoration: "none" }}>
+          <h3 className={classes.noAccountMessage}>
+            Don't have an account? Create one now.
+          </h3>
+        </Link>
+      </div>
     </UserFormPageTemplate>
   );
 };
