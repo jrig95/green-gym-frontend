c263eba6151c982d2019202f59e9ce2915ab33c1
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-07 20:31:01 +0800

    added forgot password link

--- src/Pages/UserFormPages/Login.js
+++ src/Pages/UserFormPages/Login.js
@@ -5,14 +5,20 @@ import LoginForm from "../../components/Forms/LoginForm";
 import UserFormPageTemplate from "./UserFormPageTemplate";
 
 const Login = () => {
+
+  const linkStyle = { textDecoration: "none" }
+
   return (
     <UserFormPageTemplate>
       <div className={classes.loginContainer}>
         <LoginForm />
-        <Link to="/signup" style={{ textDecoration: "none" }}>
+        <Link to="/signup" style={linkStyle}>
           <h3 className={classes.noAccountMessage}>
             Don't have an account? Create one now.
           </h3>
+        </Link>
+        <Link to="/forgot-password" style={linkStyle}>
+          <h3 className={classes.forgotPassword}>Forgot your password?</h3>
         </Link>
       </div>
     </UserFormPageTemplate>
