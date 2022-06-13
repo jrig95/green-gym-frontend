72fe6cb532e8ca83588760b18357471ea3e6fd89
Sean <seanalexclark@gmail.com>
2022-06-13 23:23:52 +0800

    added translations to landing page, error page, courousel, activities page

--- src/components/UI/CarouselMain.js
+++ src/components/UI/CarouselMain.js
@@ -17,18 +17,18 @@ const CarouselMain = () => {
 
   // Change the image, title and body here to change the info on the cards
   // Add translation to the CarouselCard directly - no need for this object
-  const carouselCardData = {
-    cardTwo: {
-      image: "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-      title: "Make a difference",
-      body: "To yourself and the world"
-    },
-    cardThree: {
-      image: "https://images.unsplash.com/photo-1528183429752-a97d0bf99b5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      title: "Plant a tree",
-      body: "Plant lasting change in your life and the world"
-    },
-  }
+  // const carouselCardData = {
+  //   cardTwo: {
+  //     image: "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  //     title: "Make a difference",
+  //     body: "To yourself and the world"
+  //   },
+  //   cardThree: {
+  //     image: "https://images.unsplash.com/photo-1528183429752-a97d0bf99b5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  //     title: "Plant a tree",
+  //     body: "Plant lasting change in your life and the world"
+  //   },
+  // }
 
 
   return (
@@ -49,16 +49,23 @@ const CarouselMain = () => {
         </SwiperSlide>
         <SwiperSlide>
           <CarouselCard
-            image={carouselCardData.cardTwo.image}
-            title={carouselCardData.cardTwo.title}
-            body={carouselCardData.cardTwo.body}
+            image="https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3ltfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            // title={carouselCardData.cardTwo.title}
+            title={t("carousel_card_two_title")}
+
+            // body={carouselCardData.cardTwo.body}
+            body={t("carousel_card_two_body")}
+
           />
         </SwiperSlide>
         <SwiperSlide>
           <CarouselCard
-            image={carouselCardData.cardThree.image}
-            title={carouselCardData.cardThree.title}
-            body={carouselCardData.cardThree.body}
+            image="https://images.unsplash.com/photo-1528183429752-a97d0bf99b5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            // title={carouselCardData.cardThree.title}
+            title={t("carousel_card_three_title")}
+            // body={carouselCardData.cardThree.body}
+            body={t("carousel_card_three_body")}
+
           />
         </SwiperSlide>
       </Swiper>
