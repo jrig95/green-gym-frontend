76e33e9aac753b6029214fb197d3ca17b5b130b6
James Riggleman <james.riggleman@yahoo.com>
2022-06-14 16:45:14 +0800

    logo, icp, qr codes, and program card truncated text

--- src/Pages/ProgramPages/PurchasePage.js
+++ src/Pages/ProgramPages/PurchasePage.js
@@ -2,13 +2,27 @@ import { Fragment } from "react";
 
 import Banner from "../../components/Layout/Banner";
 import classes from "./PurchasePage.module.css";
+import Monica from '../../assets/monicaQr.jpg'
+import Darren from '../../assets/darrenQr.jpg'
+
+
 
 const PurchasePage = () => {
   return (
     <Fragment>
       <Banner title="Purchase" />
       <div className={classes.container}>
+        <div>
         <h1>Purchase</h1>
+        </div>
+        <div className={classes.qr}>
+        <div>
+          <img src={Monica} alt="Monica Qr Code" />
+        </div>
+        <div>
+          <img src={Darren} alt="Darren Qr Code" />
+        </div>
+        </div>
       </div>
     </Fragment>
   );
