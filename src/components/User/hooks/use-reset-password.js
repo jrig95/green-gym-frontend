06c248059658d2a4e20171ea89a275fcfb761418
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-19 16:31:31 +0800

    added new object with user and key pair values

--- src/components/User/hooks/use-reset-password.js
+++ src/components/User/hooks/use-reset-password.js
@@ -5,14 +5,13 @@ import useAPIError from "../../../common/hooks/use-API-error";
 import { baseUrl } from "../../../axiosInstance/constants";
 
 const resetPassword = async (user) => {
-  await axios.post(`${baseUrl}/reset_password`, user);
+  await axios.post(`${baseUrl}/reset_password`, { user: user });
 };
 
 export const useResetPassword = () => {
   const { addError } = useAPIError();
   const { mutate, isSuccess } = useMutation((user) => resetPassword(user), {
     onError: (error) => {
-      console.log(error);
       const title =
         error instanceof Error ? error.message : "error connecting to server";
       addError(title, error.status);
