cbf897c8bd6c6f969e3efc6be6ed169f73abb3e9
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-26 14:13:42 +0800

    added BearerToken to headers in api call for user

--- src/components/AdminComponents/Members/hooks/use-members.js
+++ src/components/AdminComponents/Members/hooks/use-members.js
@@ -7,20 +7,22 @@ import { queryKeys } from "../../../../react-query/constants";
 import useAPIError from "../../../../common/hooks/use-API-error";
 import { baseUrl } from "../../../../axiosInstance/constants";
 
-const getMembers = async (searchParams, barerToken) => {
+const getMembers = async (searchParams, bearerToken) => {
   if (searchParams !== "") {
-    const { data } = await axios(`${baseUrl}/users?query=${searchParams}`);
+    const { data } = await axios(`${baseUrl}/users?query=${searchParams}`, {
+      headers: {
+        Authorization: bearerToken,
+      },
+    });
 
     return data;
   }
 
   if (searchParams === "") {
     const { data } = await axios(`${baseUrl}/users`, {
-    
-        headers: {
-          Authorization: barerToken,
-        }
-      
+      headers: {
+        Authorization: bearerToken,
+      },
     });
 
     return data;
@@ -29,12 +31,12 @@ const getMembers = async (searchParams, barerToken) => {
 
 export const useGetMembers = (searchParams) => {
   const authCtx = useContext(AuthContext);
-  const barerToken = authCtx.token;
+  const bearerToken = authCtx.token;
 
   const { addError } = useAPIError();
   const { data, isLoading, refetch, isError, error } = useQuery(
     queryKeys.members,
-    () => getMembers(searchParams, barerToken),
+    () => getMembers(searchParams, bearerToken),
     {
       onError: (error) => {
         const title =
