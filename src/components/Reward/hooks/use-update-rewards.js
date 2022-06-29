d21e5c95ae6b31d555a8348c486cf81c1c5efa15
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 19:06:38 +0800

    added bearer token to use update rewards

--- src/components/Reward/hooks/use-update-rewards.js
+++ src/components/Reward/hooks/use-update-rewards.js
@@ -1,30 +1,43 @@
 import axios from "axios";
 import { useMutation, useQueryClient } from "react-query";
+import { useContext } from "react";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { queryKeys } from "../../../react-query/constants";
 import { baseUrl } from "../../../axiosInstance/constants";
 
-const updateReward = async (updatedReward) => {
+const updateReward = async (updatedReward, bearerToken) => {
   await axios.patch(
     `${baseUrl}/rewards/${updatedReward.id}`,
-    updatedReward.rewardData
+    { reward: updatedReward.rewardData },
+    {
+      headers: {
+        Authorization: bearerToken,
+      },
+    }
   );
 };
 
 export const useUpdateReward = () => {
+  const authCtx = useContext(AuthContext);
+  const bearerToken = authCtx.token;
+
   const { addError } = useAPIError();
   const queryClient = useQueryClient();
-  const { mutate, isSuccess } = useMutation((reward) => updateReward(reward), {
-    onSuccess: () => {
-      queryClient.invalidateQueries([queryKeys.rewards]);
-    },
-    onError: (error) => {
-      const title =
-        error instanceof Error ? error.message : "error connecting to server";
-      addError(title, error.status);
-    },
-  });
+  const { mutate, isSuccess } = useMutation(
+    (reward) => updateReward(reward, bearerToken),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([queryKeys.rewards]);
+      },
+      onError: (error) => {
+        const title =
+          error instanceof Error ? error.message : "error connecting to server";
+        addError(title, error.status);
+      },
+    }
+  );
 
   return { mutate, isSuccess };
 };
