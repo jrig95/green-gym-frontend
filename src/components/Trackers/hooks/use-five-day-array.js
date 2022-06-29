3dde63034b40aaf4f8e24b320cb9cb4f566edf23
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 18:41:27 +0800

    added bearer token to use five day array

--- src/components/Trackers/hooks/use-five-day-array.js
+++ src/components/Trackers/hooks/use-five-day-array.js
@@ -1,22 +1,31 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useContext } from "react";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { baseUrl } from "../../../axiosInstance/constants";
 import { queryKeys } from "../../../react-query/constants";
 
-const getFiveDayArray = async (programTrackerId) => {
+const getFiveDayArray = async (programTrackerId, bearerToken) => {
   const { data } = await axios(
-    `${baseUrl}/program_trackers/${programTrackerId}/five_day_array`
+    `${baseUrl}/program_trackers/${programTrackerId}/five_day_array`, {
+      headers: {
+        Authorization: bearerToken
+      }
+    }
   );
 
   return data;
 };
 
 export const useFiveDayArray = (programTrackerId) => {
+  const authCtx = useContext(AuthContext);
+  const bearerToken = authCtx.token;
+
   const { addError } = useAPIError();
   const fallback = [];
-  const { data = fallback, isLoading, refetch } = useQuery(queryKeys.five_day_array, () => getFiveDayArray(programTrackerId), {
+  const { data = fallback, isLoading, refetch } = useQuery(queryKeys.five_day_array, () => getFiveDayArray(programTrackerId, bearerToken), {
     onError: (error) => {
       const title = error instanceof Error ? error.message : "error connecting to server";
       addError(title, error.status);
