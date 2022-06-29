4d445d6272037fbb70cacbe830eb1005a28d21b0
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 19:11:39 +0800

    added bearer token to use create program

--- src/components/Program/hooks/use-create-program.js
+++ src/components/Program/hooks/use-create-program.js
@@ -1,19 +1,28 @@
 import axios from "axios";
 import { useMutation, useQueryClient } from "react-query";
+import { useContext } from "react";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { queryKeys } from "../../../react-query/constants";
 import { baseUrl } from "../../../axiosInstance/constants";
 
-const createProgram = async (program) => {
-  await axios.post(`${baseUrl}/programs`, program);
+const createProgram = async (program, bearerToken) => {
+  await axios.post(`${baseUrl}/programs`, {program: program}, {
+    headers: {
+      Authorization: bearerToken
+    }
+  });
 };
 
 export const useCreateProgram = () => {
+  const authCtx = useContext(AuthContext);
+  const bearerToken = authCtx.token;
+
   const { addError } = useAPIError();
   // const fallback = [];
   const queryClient = useQueryClient();
-  const { mutate } = useMutation((program) => createProgram(program), {
+  const { mutate } = useMutation((program) => createProgram(program, bearerToken), {
     onSuccess: () => {
       queryClient.invalidateQueries([queryKeys.programs]);
     },
