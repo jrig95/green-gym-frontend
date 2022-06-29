8ba48ac4d3fe1b1b493ca25407cbc4309e18d49c
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 20:59:20 +0800

    added bearer token to use update exericse overview

--- src/components/Exercise/hooks/use-update-exercise-overview.js
+++ src/components/Exercise/hooks/use-update-exercise-overview.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useMutation, useQueryClient } from "react-query";
+import { useContext } from "react";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { queryKeys } from "../../../react-query/constants";
 import { baseUrl } from "../../../axiosInstance/constants";
 
 // /api/v1/programs/:program_id/daily_workouts/:daily_workout_id/exercise_overviews/:id
 
-const updateExerciseOverview = async (exerciseOverviewData) => {
+const updateExerciseOverview = async (exerciseOverviewData, bearerToken) => {
   const exercise_overview = {
     overview_exercise_title: exerciseOverviewData.overview_exercise_title,
     number_of_sets: exerciseOverviewData.number_of_sets,
@@ -15,15 +17,23 @@ const updateExerciseOverview = async (exerciseOverviewData) => {
 
   await axios.patch(
     `${baseUrl}/programs/${exerciseOverviewData.program_id}/daily_workouts/${exerciseOverviewData.daily_workout_id}/exercise_overviews/${exerciseOverviewData.id}`,
-    exercise_overview
+    {exercise_overview: exercise_overview},
+    {
+      headers: {
+        Authorization: bearerToken
+      }
+    }
   );
 };
 
 export const useUpdateExerciseOverview = () => {
+  const authCtx = useContext(AuthContext);
+  const bearerToken = authCtx.token;
+
   const { addError } = useAPIError();
   const queryClient = useQueryClient();
   const { mutate, isSuccess } = useMutation(
-    (exerciseOverviewData) => updateExerciseOverview(exerciseOverviewData),
+    (exerciseOverviewData) => updateExerciseOverview(exerciseOverviewData, bearerToken),
     {
       onSuccess: () => {
         queryClient.invalidateQueries([queryKeys.workout]);
