a8cfcd55323981c74bb471239403b35f59692824
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 21:07:04 +0800

    removed bearer token for use programs

--- src/components/Program/hooks/use-programs.js
+++ src/components/Program/hooks/use-programs.js
@@ -1,26 +1,22 @@
 import axios from "axios";
 import { useQuery } from "react-query";
-import { useContext } from "react";
+// import { useContext } from "react";
 
-import AuthContext from "../../../context/AuthContext";
+// import AuthContext from "../../../context/AuthContext";
 // import { axiosInstance } from "../../../axiosInstance";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { queryKeys } from "../../../react-query/constants";
 import { baseUrl } from "../../../axiosInstance/constants";
 
 // for when we need a query function for useQuery
-const getPrograms = async (bearerToken) => {
-  const { data } = await axios(`${baseUrl}/programs`, {
-    headers: {
-      Authorization: bearerToken
-    }
-  });
+const getPrograms = async () => {
+  const { data } = await axios(`${baseUrl}/programs`);
   return data;
 };
 
 export const usePrograms = () => {
-  const authCtx = useContext(AuthContext);
-  const bearerToken = authCtx.token;
+  // const authCtx = useContext(AuthContext);
+  // const bearerToken = authCtx.token;
 
   const { addError } = useAPIError();
   const fallback = [];
@@ -29,7 +25,7 @@ export const usePrograms = () => {
     isError,
     error,
     isLoading,
-  } = useQuery(queryKeys.programs, getPrograms(bearerToken), {
+  } = useQuery(queryKeys.programs, getPrograms, {
     onError: (error) => {
       const title =
         error instanceof Error ? error.message : "error connecting to server";
