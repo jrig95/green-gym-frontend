9623b5ec3d3e642c3971dbab26cd62b2f04880ee
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 19:17:45 +0800

    added bearer token to use update program

--- src/components/Program/hooks/use-update-program.js
+++ src/components/Program/hooks/use-update-program.js
@@ -1,23 +1,32 @@
 import axios from "axios";
 import { useMutation, useQueryClient } from "react-query";
+import { useContext } from "react";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { queryKeys } from "../../../react-query/constants";
 import { baseUrl } from "../../../axiosInstance/constants";
 
-const updateProgram = async (programData) => {
+const updateProgram = async (programData, bearerToken) => {
   const program = {
     program_title: programData.program_title,
     program_description: programData.program_description,
     price: programData.price,
   };
-  await axios.patch(`${baseUrl}/programs/${programData.id}`, program);
+  await axios.patch(`${baseUrl}/programs/${programData.id}`, { program: program }, {
+    headers: {
+      Authorization: bearerToken
+    }
+  });
 };
 
 export const useUpdateProgram = () => {
+  const authCtx = useContext(AuthContext);
+  const bearerToken = authCtx.token;
+
   const { addError } = useAPIError();
   const queryClient = useQueryClient();
-  const { mutate } = useMutation((programData) => updateProgram(programData), {
+  const { mutate } = useMutation((programData) => updateProgram(programData, bearerToken), {
     onSuccess: () => {
       queryClient.invalidateQueries([queryKeys.program]);
     },
