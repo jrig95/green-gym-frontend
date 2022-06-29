0b058b50963bd2c50857ee9a5c4a8e34386ef7aa
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 13:14:22 +0800

    added authorization to update profile hook

--- src/components/User/hooks/use-update-profile.js
+++ src/components/User/hooks/use-update-profile.js
@@ -1,22 +1,33 @@
 import axios from "axios";
+import { useContext } from "react";
 import { useMutation } from "react-query";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { baseUrl } from "../../../axiosInstance/constants";
 
-const updateProfile = async (user) => {
-  await axios.patch(`${baseUrl}/users/${user.id}`, { user: user });
+const updateProfile = async (user, bearerToken) => {
+  await axios.patch(`${baseUrl}/users/${user.id}`, {
+    user: user,
+    headers: { Authorization: bearerToken },
+  });
 };
 
 export const useUpdateProfile = () => {
+  const authCtx = useContext(AuthContext);
+  const bearerToken = authCtx.token;
+
   const { addError } = useAPIError();
-  const { mutate, isSuccess } = useMutation((user) => updateProfile(user), {
-    onError: (error) => {
-      const title =
-        error instanceof Error ? error.message : "error connecting to server";
-      addError(title, error.status);
-    },
-  });
+  const { mutate, isSuccess } = useMutation(
+    (user) => updateProfile(user, bearerToken),
+    {
+      onError: (error) => {
+        const title =
+          error instanceof Error ? error.message : "error connecting to server";
+        addError(title, error.status);
+      },
+    }
+  );
 
   return { mutate, isSuccess };
 };
