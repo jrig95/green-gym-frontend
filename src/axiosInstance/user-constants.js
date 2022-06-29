0d1a2ee03111390a4cb3326f16a8c2748ef9d112
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 10:48:32 +0800

    changed the base url and user base url to production url

--- src/axiosInstance/user-constants.js
+++ src/axiosInstance/user-constants.js
@@ -1,1 +1,5 @@
-export const userBaseUrl = "http://localhost:3000"
+// Use for development
+// export const userBaseUrl = "http://localhost:3000"
+
+// Use for production
+export const userBaseUrl = "http://api.greengym.com.cn"
