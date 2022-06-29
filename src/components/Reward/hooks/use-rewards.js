ccc73ec21b1249a60a61fdc6854b9c366846288d
Alex Pritchard <alexbeet1234@gmail.com>
2022-06-29 19:05:09 +0800

    added bearer token to use rewards

--- src/components/Reward/hooks/use-rewards.js
+++ src/components/Reward/hooks/use-rewards.js
@@ -1,16 +1,25 @@
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useContext } from "react";
 
+import AuthContext from "../../../context/AuthContext";
 import useAPIError from "../../../common/hooks/use-API-error";
 import { queryKeys } from "../../../react-query/constants";
 import { baseUrl } from "../../../axiosInstance/constants";
 
-const getRewards = async () => {
-  const { data } = await axios(`${baseUrl}/rewards`);
+const getRewards = async (bearerToken) => {
+  const { data } = await axios(`${baseUrl}/rewards`, {
+    headers: {
+      Authorization: bearerToken
+    }
+  });
   return data;
 };
 
 export const useRewards = () => {
+  const authCtx = useContext(AuthContext);
+  const bearerToken = authCtx.token;
+
   const { addError } = useAPIError();
 
   const fallback = [];
@@ -19,7 +28,7 @@ export const useRewards = () => {
     isError,
     error,
     isLoading,
-  } = useQuery(queryKeys.rewards, getRewards, {
+  } = useQuery(queryKeys.rewards, getRewards(bearerToken), {
     onError: (error) => {
       const title =
         error instanceof Error ? error.message : "error connecting to server";
