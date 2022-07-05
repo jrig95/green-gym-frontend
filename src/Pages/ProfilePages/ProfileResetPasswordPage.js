5c68828f7804250a5012f5cc399f23fa352c5cfc
Sean <seanalexclark@gmail.com>
2022-07-05 17:05:16 +0800

    did translations to profile page reset password, and reset password form

--- src/Pages/ProfilePages/ProfileResetPasswordPage.js
+++ src/Pages/ProfilePages/ProfileResetPasswordPage.js
@@ -3,11 +3,14 @@ import { Fragment } from "react";
 import classes from './ProfileResetPasswordPage.module.css';
 import Banner from "../../components/Layout/Banner";
 import ProfileResetPasswordForm from "../../components/Forms/ProfileResetPasswordForm";
+import { useTranslation } from "react-i18next";
 
 const ProfileResetPasswordPage = () => {
+  const { t } = useTranslation();
+
   return (
     <Fragment>
-      <Banner title="My Profile" />
+      <Banner title={t("profile_reset_password_page_my_profile")}/>
       <div className={classes.formContainer}>
         <ProfileResetPasswordForm />
       </div>
