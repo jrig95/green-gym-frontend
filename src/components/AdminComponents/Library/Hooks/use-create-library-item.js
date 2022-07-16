4044e196d853f0a3626f27d9e86940daaef6cf3c
Alex Pritchard <alexbeet1234@gmail.com>
2022-07-16 16:54:25 +0800

    added authorization to create library item

--- src/components/AdminComponents/Library/Hooks/use-create-library-item.js
+++ src/components/AdminComponents/Library/Hooks/use-create-library-item.js
@@ -1,17 +1,26 @@
 import axios from "axios";
 import { useMutation, useQueryClient } from "react-query";
+import { useContext } from "react";
 
+import AuthContext from "../../../../context/AuthContext";
 import { queryKeys } from "../../../../react-query/constants";
 import { baseUrl } from "../../../../axiosInstance/constants";
 
-const createLibraryItem = async (libraryItem) => {
-  await axios.post(`${baseUrl}/library_items`, libraryItem);
+const createLibraryItem = async (libraryItem, barerToken) => {
+  await axios.post(`${baseUrl}/library_items`, libraryItem, {
+    headers: {
+      Authorization: barerToken
+    }
+  });
 };
 
 export const useCreateLibraryItem = () => {
+  const authCtx = useContext(AuthContext);
+  const barerToken = authCtx;
+
   const queryClient = useQueryClient();
   const { mutate } = useMutation(
-    (libraryItem) => createLibraryItem(libraryItem),
+    (libraryItem) => () => createLibraryItem(libraryItem, barerToken),
     {
       onSuccess: () => {
         queryClient.invalidateQueries([queryKeys.libraryItems]);
