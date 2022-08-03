26432452e323ba91d3242174b7202d9a85cb324d
James Riggleman <james.riggleman@yahoo.com>
2022-08-03 18:57:27 +0800

    updated user-constants

--- src/axiosInstance/constants.js
+++ src/axiosInstance/constants.js
@@ -1,5 +1,5 @@
 // Use for development
-export const baseUrl = "http://localhost:3000/api/v1";
+// export const baseUrl = "http://localhost:3000/api/v1";
 
 // Use for production
-// export const baseUrl = "https://api.greengym.com.cn/api/v1"
+export const baseUrl = "https://api.greengym.com.cn/api/v1"
