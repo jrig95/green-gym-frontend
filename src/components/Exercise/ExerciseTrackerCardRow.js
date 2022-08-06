80f39689be9f49d14863173a8587613dc072b150
Alex Pritchard <alexbeet1234@gmail.com>
2022-08-06 11:42:45 +0100

    removed strict mode from index

--- src/components/Exercise/ExerciseTrackerCardRow.js
+++ src/components/Exercise/ExerciseTrackerCardRow.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useInput from "../Forms/Hooks/use-input";
 import classes from "./ExerciseTrackerCard.module.css";
 import { useTranslation } from "react-i18next";
@@ -52,10 +52,12 @@ const ExerciseTrackerCardRow = ({
     increaseNumberOfExercisesComplete();
   };
 
-  if (!exerciseQuestion) {
-    increaseNumberOfExercisesComplete();
-  }
-
+  useEffect(() => {
+    if (!exerciseQuestion) {
+      increaseNumberOfExercisesComplete();
+    }
+  }, [])
+  
   return (
     <div className={rowClasses}>
       <p className={classes.exerciseRow}>{exercise}</p>
