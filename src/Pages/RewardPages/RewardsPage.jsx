32c66850b0725f881932ad1d702e21873648e6e7
MarcoZeee <MarcoZeee@github.com>
2022-11-23 18:58:38 +0800

    feat: program layout

--- src/Pages/RewardPages/RewardsPage.jsx
+++ src/Pages/RewardPages/RewardsPage.jsx
@@ -6,11 +6,12 @@ import { useUser } from "../../components/User/hooks/use-user";
 
 const RewardsPage = () => {
   const authCtx = useContext(AuthContext);
-  const { data: userData, isLoading: userIsLoading } = useUser(authCtx.userId);
+  const { data: userData, isLoading: userIsLoading, isError } = useUser(authCtx.userId);
 
   const admin = authCtx.isAdmin;
 
   if (userIsLoading) return <p>Loading</p>;
+  if(isError) return <p>Error</p>;
 
   const noProgram = userData.programs.length === 0;
 
